24dfdff2630f209cb0cb162f5195813a777b1248
insideoutzombie <david@web3devs.com>
2019-09-30 14:21:53 -0500

    problem 6

--- q6.py
+++ q6.py
@@ -1,16 +1,4 @@
 # ### Problem 6
-# Create a class called ClubMember 
-# Each club member has a name and a role  
-# Create ClubMember instances for the following people:
-# ```
-# Alfred - Club President
-# Troy - Club Vice President
-# Albert - Club Secretary
-# Bob - Club Treasurer
-# ```
-# Add each member instance to a new club_members list that you create.
-# Write the code needed to loop through the club member list and print the current number of members in the list, then the member’s name and club role, one per line using f strings.
-
 # Example Output:
 # ```
 # There are currently 4 club members in the list!
@@ -21,3 +9,37 @@
 # Club Treasurer: Bob
 # ```
 
+# Create a class called ClubMember
+# Each club member has a name and a role
+class ClubMember:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+# Create ClubMember instances for the following people:
+member1 = ClubMember("Alfred", "Club President")
+member2 = ClubMember("Troy", "Club Vice President")
+member3 = ClubMember("Albert", "Club Secretary")
+member4 = ClubMember("Bob", "Club Treasurer")
+
+# Add each member instance to a new club_members list that you create.
+club_members = []
+
+# Write the code needed to loop through the club member list and print
+# the current number of members in the list, then the member’s name and
+# club role, one per line using f strings.
+for x in member1:
+    x.append(club_members)
+
+    print(x.name)
+
+
+
+
+
+
+
+
+
+
+# TODO: research TypeError: 'ClubMember' object is not iterable
