ca3afe765619aa3ba2dd211cff9bc3b3af773fd3
insideoutzombie <david@web3devs.com>
2019-09-30 14:20:05 -0500

    q1 commit

--- q1.py
+++ q1.py
@@ -1,7 +1,4 @@
 # ### Problem 1
-# Ask the user to enter a number. 
-# Using the provided list of numbers, use a for loop to iterate the array and print out all the values that are smaller than the user input and print out all the values that are larger than the number entered by the user.
-
 # ```
 # # Start with this List
 # list_of_many_numbers = [12, 24, 1, 34, 10, 2, 7]
@@ -12,3 +9,19 @@
 # 1  2  7 are smaller than 9
 # 12  24  34  10 are larger than 9
 # ```
+
+# Ask the user to enter a number.
+userInput = int(input("Enter a number "))
+
+# Using the provided list of numbers, use a for loop to iterate the array and
+# print out all the values that are smaller than the user input and print out
+# all the values that are larger than the number entered by the user.
+list_of_many_numbers = [12, 24, 1, 34, 10, 2, 7]
+
+for x in list_of_many_numbers:
+    # print out all the values that are smaller than the user input
+    if (userInput > x):
+        print('the value that is smaller than the user input ', x)
+    # print out all the values that are larger than the number entered by the user
+    if (userInput < x):
+        print('the value that is greater than the user input ', x)
