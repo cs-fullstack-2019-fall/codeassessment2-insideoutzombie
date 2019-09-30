c62885ad4ae8d82a6fe9687b18b7cfcc0a822cc5
insideoutzombie <david@web3devs.com>
2019-09-30 14:20:34 -0500

    problem2 commit

--- q2.py
+++ q2.py
@@ -1,5 +1,12 @@
 # ### Problem 2
-# Prompt the user with the message, ‘Is it better to be rude or kind to People?’ 
-# Keeping prompting the user to enter an answer until they enter the word kind. 
-# Each time they enter something other than kind, print the message, ‘That’s not the answer I had hoped to hear. Try again.’ and prompt the user again.
-# Once the user enters kind, print, ’Now that’s what I wanted to hear!’ and exit the program.
+
+# Prompt the user with the message, ‘Is it better to be rude or kind to People?’
+userInput = input('Is it better to be rude or kind to People? ')
+
+# Keep prompting the user to enter an answer until they enter the word kind.
+while(userInput != 'kind'):
+    # Each time they enter something other than kind, print the message,
+    # ‘That’s not the answer I had hoped to hear. Try again.’ and prompt the user again.
+    userInput = input('That’s not the answer I had hoped to hear. Try again. ')
+# Once the user enters kind, print, ’Now that’s what I wanted to hear!’ and exit the programself.
+print('Now that’s what I wanted to hear!')
